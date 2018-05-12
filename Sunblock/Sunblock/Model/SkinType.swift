a691fc455ab9ab963940052591f83828d679869e
Unknown <vikram.sh86@gmail.com>
2018-05-12 20:23:04 +0200

    Updated SkinType with sun exposure time and descriptions

--- Sunblock/Sunblock/Model/SkinType.swift
+++ Sunblock/Sunblock/Model/SkinType.swift
@@ -34,4 +34,49 @@ enum SkinType: String {
             type6
         ]
     }
+
+    var maximumTimeInSun: Int {
+        switch self {
+        case .type1: return 67
+        case .type2: return 100
+        case .type3: return 200
+        case .type4: return 300
+        case .type5: return 400
+        case .type6: return 500
+        }
+    }
+
+    var skinDescription: String {
+        switch self {
+        case .type1:
+            return "Never tans, always burns easily, skin particularly light, freckles, redish hair. (All babies and children.)"
+        case .type2:
+            return "Freckles rare, tans slightly, high inclination to sunburn"
+        case .type3:
+            return "Skin light/light brown, no freckles, good tanning ability, very low inclination to sunburn."
+        case .type4:
+            return "Skin light-brown to olive, no freckles, very good tanning ability, very low inclination to sunburn"
+        case .type5:
+            return "Olive skin  in color, sun insensitive skin, very low inclination to sunburn"
+        case .type6:
+            return "Skin deeply pigmented, sun insensitive skin, never burns"
+        }
+    }
+
+    var geneticOrigin: String {
+        switch self{
+        case .type1:
+            return "Scandinavian, Celtic"
+        case .type2:
+            return "Caucasians"
+        case .type3:
+            return "Central Europe"
+        case .type4:
+            return "South Mediteranean, South American"
+        case .type5:
+            return "Middle Eastern, Asia, some Hispanics and Afro-American"
+        case .type6:
+            return "African, Afro-American"
+        }
+    }
 }
--- Sunblock/Sunblock/Views/SegmentChooserView/SegmentChooserView.swift
+++ Sunblock/Sunblock/Views/SegmentChooserView/SegmentChooserView.swift
@@ -6,4 +6,63 @@
 //  Copyright © 2018 Shortcut. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable class SegmentChooserView: UIView, NibFileOwnerLoadable{
+
+    @IBOutlet private weak var firstDimmerView: UIView!
+    @IBOutlet private weak var secondDimmerView: UIView!
+    @IBOutlet private weak var thirdDimmerView: UIView!
+
+    @IBInspectable var selectedIndex = 0 {
+        didSet{
+            updateSelection(for: selectedIndex)
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+         setup()
+    }
+
+    private func setup(){
+        loadNibContent()
+        selectedIndex = 1
+    }
+
+}
+
+//MARK: Private functions
+extension SegmentChooserView{
+    private func updateSelection(for index: Int){
+        UIView.animate(withDuration: 0.33) {
+            if index == 0 {
+                self.firstDimmerView.alpha	= 0
+                self.secondDimmerView.alpha = 0.75
+                self.thirdDimmerView.alpha 	= 0.75
+            }else if index == 1{
+                self.firstDimmerView.alpha	= 0.75
+                self.secondDimmerView.alpha	= 0
+                self.thirdDimmerView.alpha	= 0.75
+            }else{
+                self.firstDimmerView.alpha	= 0.75
+                self.secondDimmerView.alpha	= 0.75
+                self.thirdDimmerView.alpha	= 0
+            }
+        }
+    }
+}
+
+//MARK: IBActions
+extension SegmentChooserView{
+
+    @IBAction private func segmentSelected(_ sender: UIButton) {
+        print("\(sender.tag) is selected!")
+        selectedIndex = sender.tag
+    }
+}
